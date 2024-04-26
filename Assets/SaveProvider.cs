4d81bd87d4f06812788142214426ca187ab61ea4
Dmitry Starokozhev <p1cachu@yandex.ru>
2024-04-26 21:38:26 +0300

    Card shirts assets

---
+++ Assets/SaveProvider.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveProvider : MonoBehaviour
+{
+	private SaveData _data;
+	private ISaver<SaveData> _saver;
+
+	public List<string> Cards => _data.Cards;
+
+	private void Awake()
+	{
+		_saver = new JsonSaver();
+		_data = _saver.Load();
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void ChangeCardState(string id, bool state)
+	{
+		if (state && !GetCardState(id))
+		{
+			_data.Cards.Add(id);
+		}
+		else
+		{
+			_data.Cards.Remove(id);
+		}
+
+		_saver.Save(_data);
+	}
+
+	public bool GetCardState(string id)
+	{
+		return _data != null && _data.Cards.Count > 0 && _data.Cards.Exists(s => s.Equals(id));
+	}
+
+	public int GetCardsCount()
+	{
+		return _data.Cards.Count;
+	}
+}
